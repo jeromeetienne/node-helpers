48a7801b16e1c21a34672599185377005454ca28
jerome <jerome.etienne@gmail.com>
2010-06-23 10:18:28 +0200

    tty_color: provide ansi escape code 	new file:   ez_tty_color.js

---
+++ ez_tty_color.js
@@ -0,0 +1,12 @@
+# some ansi tty color - http://en.wikipedia.org/wiki/ANSI_escape_code
+
+exports.all_off		= "\e[0m";
+exports.bold		= "\e[1m";
+exports.faint   	= "\e[2m"
+exports.cls		= "\e[2J\e[0;0H"
+exports.fg_green	= "\e[32m"
+exports.fg_red		= "\e[31m"
+exports.bg_green	= "\e[42m"
+exports.bg_red		= "\e[41m"
+exports.img_neg		= "\e[7m"
+exports.img_pos		= "\e[27m"
